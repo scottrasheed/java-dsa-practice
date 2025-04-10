01bdcd2bab64b2fa83261b7ee3d6451205cd9c3d
Scott Rasheed <scottrasheed@ymail.com>
2025-04-10 19:23:32 -0400

    Create script.js

---
+++ Udemy-Course-Notes/Basics/script.js
@@ -0,0 +1,7 @@
+const message = 'Hello world' // Define a message
+
+// Update header text in the HTML
+document.querySelector('#header').innerHTML = message
+
+// Log message to the browser console
+console.log(message)
