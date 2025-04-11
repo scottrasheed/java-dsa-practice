50d7ac33ba7b387ac5ae1933aea546a8ed0a2c8d
Scott Rasheed <scottrasheed@ymail.com>
2025-04-10 22:00:00 -0400

    Update LinkedList.java

--- Udemy-Course-Notes/Basics/LinkedList.java
+++ Udemy-Course-Notes/Basics/LinkedList.java
@@ -1,42 +1,19 @@
-public class LinkedList {
-  Node head;
-  Node tail;
-  int length;
+public LinkedList(int value) {
+  Node newNode = new Node(value);
+  head = newNode;
+  tail = newNode;
+  length = 1;
+}
 
-  public LinkedList() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
 
-  public void append(int value) {
-    Node newNode = new Node(value);
-    if (head == null) {
-      head = newNode;
-      tail = newNode;
-    } else {
+public void append(int value) {
+  Node newNode = new Node (value);
+  if (head == null) {
+    head = newNode;
+    tail = newNode;
+    }else{
       tail.next = newNode;
       tail = newNode;
     }
     length++;
-  }
-
-  public void printList() {
-    Node current = head;
-    while (current != null) {
-      System.out.print(current.value + " -> ");
-      current = current.next;
-    }
-    System.out.println("null");
-  }
-}
-
-class Node {
-  int value;
-  Node next;
-
-  public Node(int value) {
-    this.value = value;
-    this.next = null;
-  }
 }
