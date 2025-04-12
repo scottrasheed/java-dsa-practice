5ded4c65a2c2463fb82df6ec6fca4f0aa5241a63
Scott Rasheed <scottrasheed@ymail.com>
2025-04-11 20:07:01 -0400

    Delete Udemy-Course-Notes/Basics/script.js

--- Udemy-Course-Notes/Basics/script.js
+++
@@ -1,7 +0,0 @@
-const message = 'Hello world' // Define a message
-
-// Update header text in the HTML
-document.querySelector('#header').innerHTML = message
-
-// Log message to the browser console
-console.log(message)
