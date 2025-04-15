7efb4b3fc945709a5868dd6ac2f1791d85698734
Scott Rasheed <scottrasheed@ymail.com>
2025-04-15 15:27:39 -0400

    Update and rename Linked List: Remove Last1.java to Linked List: Remove Last.java

--- Udemy-Course-Notes/Section 4: Linked Lists/Linked List: Remove Last.java
+++ Udemy-Course-Notes/Section 4: Linked Lists/Linked List: Remove Last.java
@@ -1,38 +1,75 @@
 public class Main {
 
     public static void main(String[] args) {
+
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
 
         // (2) Items - Returns 2 Node
         System.out.println(myLinkedList.removeLast().value);
-        // (1) Item - Returns 1 Node 
+        // (1) Item - Returns 1 Node
         System.out.println(myLinkedList.removeLast().value);
-        // (0) items - Returns null 
+        // (0) Items - Returns null
         System.out.println(myLinkedList.removeLast());
-    }
-}
-
-class Node {
-    int value;
-    Node next;
-
-    Node(int value) {
-        this.value = value;
-        this.next = null;
+        
+        /*
+        	EXPECTED OUTPUT:
+        	----------------
+        	2
+        	1
+        	null
+        */
     }
 }
 
 class LinkedList {
+
     private Node head;
     private Node tail;
     private int length;
 
-    LinkedList(int value) {
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length); // This line was fixed
     }
 
     public void append(int value) {
