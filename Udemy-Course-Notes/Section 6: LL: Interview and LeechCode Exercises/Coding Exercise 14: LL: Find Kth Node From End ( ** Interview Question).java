482cafff8409e93b2445da9069c7cce4852e8aff
Scott Rasheed <scottrasheed@ymail.com>
2025-04-21 21:41:12 -0400

    Create Coding Exercise 14: LL: Find Kth Node From End ( ** Interview Question).java

---
+++ Udemy-Course-Notes/Section 6: LL: Interview and LeechCode Exercises/Coding Exercise 14: LL: Find Kth Node From End ( ** Interview Question).java
@@ -0,0 +1,106 @@
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node findKthFromEnd(int k) {
+        if (head == null || k <= 0) return null;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Move fast k steps ahead
+        for (int i = 0; i < k; i++) {
+            if (fast == null) return null; // k is greater than the length of the list
+            fast = fast.next;
+        }
+
+        // Move both pointers until fast reaches the end
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+
+        int k = 2;
+        LinkedList.Node resultNode = myLinkedList.findKthFromEnd(k);
+
+        if (resultNode != null) {
+            System.out.println(resultNode.value); // Expected: 4
+        } else {
+            System.out.println("Invalid k");
+        }
+    }
+}
