e712e24ab99519b4238237d7c244342e30383b01
Scott Rasheed <scottrasheed@ymail.com>
2025-04-26 18:32:06 -0400

    Create Coding Exercise 35: Queue: Constructor.java

---
+++ Udemy-Course-Notes/Section 11: S&Q: Coding Exercises/Coding Exercise 35: Queue: Constructor.java
@@ -0,0 +1,84 @@
+public class Queue {
+
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("First: null");
+            System.out.println("Last: null");
+        } else {
+            System.out.println("First: " + first.value);
+            System.out.println("Last: " + last.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nQueue:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        first = null;
+        last = null;
+        length = 0;
+    }
+
+    public static void main(String[] args) {
+        // Create a Queue with an initial value of 4
+        Queue myQueue = new Queue(4);
+
+        // Print out the queue details
+        myQueue.printAll();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            First: 4
+            Last: 4
+            Length: 1
+            
+            Queue:
+            4
+        */
+    }
+}
