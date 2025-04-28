dc6d137a0bdcfceee11b06a16d2d912d8a6f5a21
Scott Rasheed <scottrasheed@ymail.com>
2025-04-28 14:05:02 -0400

    Create BST: Contains - Code.java

---
+++ Udemy-Course-Notes/Section 13: Trees/BST: Contains - Code.java
@@ -0,0 +1,107 @@
+// THIS GOES IN YOUR MAIN CLASS TO TEST YOUR CODE:
+// -----------------------------------------------
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.contains(27));
+
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.contains(17));
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            BST Contains 27:
+            true
+            
+            BST Contains 17:
+            false
+
+        */
+
+    }
+
+}
+
+
+
+
+
+// THIS CODE GOES IN YOUR BST CLASS:
+// ---------------------------------
+      
+
+
+
+public class BinarySearchTree {
+
+    private Node root;
+
+	class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
