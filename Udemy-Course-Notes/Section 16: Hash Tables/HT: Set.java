890d3ddaeb78e9fca39b865d177986b2f5aaf022
Scott Rasheed <scottrasheed@ymail.com>
2025-04-28 19:27:08 -0400

    Create HT: Set.java

---
+++ Udemy-Course-Notes/Section 16: Hash Tables/HT: Set.java
@@ -0,0 +1,110 @@
+// THIS GOES IN YOUR MAIN CLASS TO TEST YOUR CODE:
+// -----------------------------------------------
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
+        myHashTable.printTable();
+        
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            0:
+            1:
+            2:
+            3:
+               {screws= 140}
+            4:
+               {bolts= 200}
+            5:
+            6:
+               {nails= 100}
+               {tile= 50}
+               {lumber= 80}
+
+        */
+        
+    }
+
+}
+
+
+
+
+
+// THIS CODE GOES IN YOUR HASHTABLE CLASS:
+// ---------------------------------------
+      
+
+public class HashTable {
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node {
+        String key;
+        int value;
+        Node next;
+
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+	
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            if (temp.key == key) {
+                temp.value += value;
+                return;
+            }
+            while (temp.next != null) {
+                temp = temp.next;
+                if (temp.key == key) {
+                    temp.value += value;
+                    return;
+                }
+            }
+            temp.next = newNode;
+        }
+    }
+
+}
