c23bcfc6d8486f80afb5a00380ef3dbbb90a4dcf
Scott Rasheed <scottrasheed@ymail.com>
2025-04-30 17:22:48 -0400

    Create Coding Exercise 52: HT: Keys.java

---
+++ Udemy-Course-Notes/Section 17: HT: Coding Exercises/Coding Exercise 52: HT: Keys.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creating the HashTable object
+        HashTable myHashTable = new HashTable();
+
+        // Adding key-value pairs to the HashTable
+        myHashTable.set("paint", 20);
+        myHashTable.set("bolts", 40);
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+
+        // Printing all keys in the hash table
+        System.out.println(myHashTable.keys());
+
+        /*
+        	EXPECTED OUTPUT:
+        	----------------
+        	[paint, bolts, nails, tile, lumber]
+        */        
+
+    }
+
+    public static class HashTable {
+        private int size = 7;
+        private Node[] dataMap;
+
+        class Node {
+            String key;
+            int value;
+            Node next;
+
+            Node(String key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        public HashTable() {
+            dataMap = new Node[size];
+        }
+
+        public Node[] getDataMap() {
+            return dataMap;
+        }
+
+        public void printTable() {
+            for (int i = 0; i < dataMap.length; i++) {
+                System.out.println(i + ":");
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                    temp = temp.next;
+                }
+            }
+        }
+
+        private int hash(String key) {
+            int hash = 0;
+            char[] keyChars = key.toCharArray();
+            for (int i = 0; i < keyChars.length; i++) {
+                int asciiValue = keyChars[i];
+                hash = (hash + asciiValue * 23) % dataMap.length;
+            }
+            return hash;
+        }
+
+        public void set(String key, int value) {
+            int index = hash(key);
+            Node newNode = new Node(key, value);
+            if (dataMap[index] == null) {
+                dataMap[index] = newNode;
+            } else {
+                Node temp = dataMap[index];
+                if (temp.key.equals(key)) {
+                    temp.value += value;
+                    return;
+                }
+                while (temp.next != null) {
+                    temp = temp.next;
+                    if (temp.key.equals(key)) {
+                        temp.value += value;
+                        return;
+                    }
+                }
+                temp.next = newNode;
+            }
+        }
+
+        public int get(String key) {
+            int index = hash(key);
+            Node temp = dataMap[index];
+            while (temp != null) {
+                if (temp.key.equals(key)) return temp.value;
+                temp = temp.next;
+            }
+            return 0;
+        }
+
+        // KEYS method
+        public ArrayList<String> keys() {
+            ArrayList<String> keysList = new ArrayList<>();
+            for (int i = 0; i < dataMap.length; i++) {
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    keysList.add(temp.key);
+                    temp = temp.next;
+                }
+            }
+            return keysList;
+        }
+    }
+}
