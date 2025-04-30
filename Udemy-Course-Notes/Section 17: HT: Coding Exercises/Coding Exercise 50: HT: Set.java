72d4c12bc1823ed0b89d925794d26c362d759e75
Scott Rasheed <scottrasheed@ymail.com>
2025-04-30 17:17:56 -0400

    Create Coding Exercise 50: HT: Set.java

---
+++ Udemy-Course-Notes/Section 17: HT: Coding Exercises/Coding Exercise 50: HT: Set.java
@@ -0,0 +1,89 @@
+public class HashTable {
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node {
+        String key;
+        int value;
+        Node next;
+
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+    
+    public Node[] getDataMap() {
+        return dataMap;
+    }
+    
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+	
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Here is the main method INSIDE the public HashTable class
+    public static void main(String[] args) {
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
+        myHashTable.printTable();
+        
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            0:
+            1:
+            2:
+            3:
+               {screws= 140}
+            4:
+               {bolts= 200}
+            5:
+            6:
+               {nails= 100}
+               {tile= 50}
+               {lumber= 80}
+        */
+    }
+}
