c13bc6f8310b812a6c1da2a246aaae19b1f0c3c3
Scott Rasheed <scottrasheed@ymail.com>
2025-05-07 02:56:08 -0400

    Create Coding Exercise 76: rBST: Contains.java

---
+++ Udemy-Course-Notes/Section 26: rBST: Coding Exercises/Coding Exercise 76: rBST: Contains.java
@@ -0,0 +1,104 @@
+class BinarySearchTree {
+
+    private Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Recursive contains method
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+        if (value == currentNode.value) return true;
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.rContains(27));
+
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.rContains(17));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            BST Contains 27:
+            true
+
+            BST Contains 17:
+            false
+        */
+    }
+}
