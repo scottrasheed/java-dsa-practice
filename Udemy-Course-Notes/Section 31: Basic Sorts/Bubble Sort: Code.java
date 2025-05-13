3e7aa1fa0ed729220a67f110dbda4095fb33e490
Scott Rasheed <scottrasheed@ymail.com>
2025-05-13 00:18:29 -0400

    Update Bubble Sort: Code.java

--- Udemy-Course-Notes/Section 31: Basic Sorts/Bubble Sort: Code.java
+++ Udemy-Course-Notes/Section 31: Basic Sorts/Bubble Sort: Code.java
@@ -1,37 +1,38 @@
+package algorithms.sorts.bubblesort;
+
 import java.util.Arrays;
 
 public class BubbleSort {
 
-    // Bubble sort algorithm
     public static void bubbleSort(int[] array) {
-        boolean swapped;
-        for (int i = 0; i < array.length - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    // swap elements
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j+1]) {
                     int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                 }
             }
-            if (!swapped) break;
         }
     }
 
+
+
     public static void main(String[] args) {
 
-        int[] myArray = {4, 2, 6, 5, 1, 3};
+        int[] myArray = {4,2,6,5,1,3};
 
         bubbleSort(myArray);
 
-        System.out.println(Arrays.toString(myArray));
+        System.out.println( Arrays.toString(myArray) );
 
         /*
             EXPECTED OUTPUT:
             ----------------
             [1, 2, 3, 4, 5, 6]
+
          */
+
     }
+
 }
