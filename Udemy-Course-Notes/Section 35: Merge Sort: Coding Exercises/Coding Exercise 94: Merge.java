f78cf6251e9c3d9db0275dc56dc6d8042d4ebf09
Scott Rasheed <scottrasheed@ymail.com>
2025-05-17 02:21:54 -0400

    Create Coding Exercise 94: Merge.java

---
+++ Udemy-Course-Notes/Section 35: Merge Sort: Coding Exercises/Coding Exercise 94: Merge.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+
+public class Main {
+
+public static int[] merge(int[] array1, int[] array2) {
+    int[] merged = new int[array1.length + array2.length];
+    int i = 0, j = 0, k = 0;
+
+    while (i < array1.length && j < array2.length) {
+        if (array1[i] < array2[j]) {
+            merged[k++] = array1[i++];
+        } else {
+            merged[k++] = array2[j++];
+        }
+    }
+
+    while (i < array1.length) {
+        merged[k++] = array1[i++];
+    }
+
+    while (j < array2.length) {
+        merged[k++] = array2[j++];
+    }
+
+    return merged;
+}
+
+
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1,3,7,8};
+        int[] array2 = {2,4,5,6};
+
+        System.out.println( Arrays.toString( merge(array1, array2) ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7, 8]
+
+         */
+
+    }
+
+}
+
